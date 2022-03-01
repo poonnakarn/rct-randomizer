81b34e66159e4d166511c02b2a95baf010f64acb
Poonnakarn Panjasriprakarn <poonnakarn@gmail.com>
2022-03-01 14:57:24 +0700

    Edit admin panel

--- registry/admin.py
+++ registry/admin.py
@@ -4,6 +4,10 @@ from .models import Sequence, Patient
 class EnrollTime(admin.ModelAdmin):
     readonly_fields = ('time',)
 
-admin.site.register(Sequence)
+# admin.site.register(Sequence)
 admin.site.register(Patient, EnrollTime)
 # admin.site.register(Patient)
+
+@admin.register(Sequence)
+class SequenceAdmin(admin.ModelAdmin):
+    list_display=['arm', 'patient', 'time']
